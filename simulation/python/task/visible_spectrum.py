5bd4f3ff9c133f5f5fa7c5715ca6f35b16dc9bc7
YMTheory <932899789@qq.com>
2023-04-23 13:58:02 +0800

    Use vectorize in numpy to construct ufunc

--- simulation/python/task/visible_spectrum.py
+++ simulation/python/task/visible_spectrum.py
@@ -2,25 +2,33 @@ import numpy as np
 
 from xsec import IBD_XS
 from flux import Nakazato_2013_Model
+from detector import toyDetector
 
-from snewpy.neutrino import Flavor, MassHierarchy
-from snewpy.flavor_transformation import NoTransformation, AdiabaticMSW, ThreeFlavorDecoherence
+from snewpy.neutrino import Flavor
 
 def integrate():
 
-    model_cls = Nakazato_2013_Model()
+    model_cls = Nakazato_2013_Model.Nakazato_2013_Model()
     model_cls.initialize_model()
 
-    ibd = IBD_XS()
-    
-    def func_noDet(t, Ev):
-        return ibd.totXS(Ev) * model_cls.get_arrvied_neutrino_flux()
-    
-    Emin, Emax = 0.8, 80
-    E = np.linspace(Emin, Emax, 1000)
-    tmin, tmax = -0.03, 0.07
-    t = np.linspace(tmin, tmax, 100)
+    ibd = IBD_XS.IBD_XS()
 
-    result = np.trapz(func_noDetc, E, axis=0)
+    det = toyDetector.toyDetector(20, 0.12, 0.88)
+    
+    def func_noDet_NO(t, Ev):
+        return ibd.totXS(Ev, Flavor.NU_E_BAR) * model_cls.get_arrvied_neutrino_flux_NO_NU_E_BAR(t, Ev) * det.nH
+    ufunc_noDet_NO = np.vectorize(func_noDet_NO)
 
-    print(result)
+    tmp_val = ufunc_noDet_NO(0.02, 10)
+
+    print(f"Middle check variable = {tmp_val:.2f}.")
+    
+    #Emin, Emax = 0.8, 80
+    #E = np.linspace(Emin, Emax, 1000)
+    #tmin, tmax = -0.03, 0.07
+    #t = np.linspace(tmin, tmax, 100)
+
+    #f = ufunc_noDet_NO(t, E)
+    #print(f)
+    #result = np.trapz(func_noDet_NO, E, axis=0)
+
