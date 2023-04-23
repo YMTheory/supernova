cdca1effa570d5c770c8eb7400836575c2f3bf51
YMTheory <932899789@qq.com>
2023-04-23 13:55:55 +0800

    Fix bugs in IBD_XS: NU_E_BAR is required.

--- simulation/python/xsec/IBD_XS.py
+++ simulation/python/xsec/IBD_XS.py
@@ -14,16 +14,18 @@ class IBD_XS(reaction_XS) :
         """
         return IBD xs: unit cm2
         """
-        if flavor.name == "NU_E"
-        Ethr = 1.8022
-        if Ev < Ethr:
+        if flavor.name != "NU_E_BAR":
             return 0
         else:
-            mn = 939.57
-            mp = 938.27
-            delta_mnp = mn - mp
-            me = 0.511
-            Ee = Ev - delta_mnp
-            Pe = np.sqrt(Ee**2 - me**2)
-            totxs = 0.0952 * (Ee*Pe) * 1e-42 
-            return totxs
+            Ethr = 1.8022
+            if Ev < Ethr:
+                return 0
+            else:
+                mn = 939.57
+                mp = 938.27
+                delta_mnp = mn - mp
+                me = 0.511
+                Ee = Ev - delta_mnp
+                Pe = np.sqrt(Ee**2 - me**2)
+                totxs = 0.0952 * (Ee*Pe) * 1e-42 
+                return totxs
