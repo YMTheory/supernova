bf21dd5644cbe120ad61d5e83c3c7cbc4af1407d
YMTheory <932899789@qq.com>
2023-04-23 13:57:09 +0800

    Fix bugs in Nakazota model: mh is a keyword argument for flavor conversion in snewpy

--- simulation/python/flux/CCSNeModel.py
+++ simulation/python/flux/CCSNeModel.py
@@ -14,12 +14,71 @@ class CCSNeModel(ABC):
         pass
 
     @abstractmethod
-    def get_flux_ET(self, t, Ev, flavor):
+    def get_flux_ET_NU_E(self, t, Ev):
         return 0.
 
     @abstractmethod
-    def get_arrvied_neutrino_flux(self, t, Ev, distance, flavor, transfrom):
+    def get_flux_ET_NU_E_BAR(self, t, Ev):
         return 0.
+
+    @abstractmethod
+    def get_flux_ET_NU_X(self, t, Ev):
+        return 0.
+
+    @abstractmethod
+    def get_flux_ET_NU_X_BAR(self, t, Ev):
+        return 0.
+
+    @abstractmethod
+    def get_arrvied_neutrino_flux_noosc_NU_E(self, t, Ev):
+        return 0.
+
+    @abstractmethod
+    def get_arrvied_neutrino_flux_noosc_NU_E_BAR(self, t, Ev):
+        return 0.
+
+    @abstractmethod
+    def get_arrvied_neutrino_flux_noosc_NU_X(self, t, Ev):
+        return 0.
+
+    @abstractmethod
+    def get_arrvied_neutrino_flux_noosc_NU_X_BAR(self, t, Ev):
+        return 0.
+
+
+    @abstractmethod
+    def get_arrvied_neutrino_flux_NO_NU_E(self, t, Ev):
+        return 0.
+
+    @abstractmethod
+    def get_arrvied_neutrino_flux_NO_NU_E_BAR(self, t, Ev):
+        return 0.
+
+    @abstractmethod
+    def get_arrvied_neutrino_flux_NO_NU_X(self, t, Ev):
+        return 0.
+
+    @abstractmethod
+    def get_arrvied_neutrino_flux_NO_NU_X_BAR(self, t, Ev):
+        return 0.
+
+
+    @abstractmethod
+    def get_arrvied_neutrino_flux_IO_NU_E(self, t, Ev):
+        return 0.
+
+    @abstractmethod
+    def get_arrvied_neutrino_flux_IO_NU_E_BAR(self, t, Ev):
+        return 0.
+
+    @abstractmethod
+    def get_arrvied_neutrino_flux_IO_NU_X(self, t, Ev):
+        return 0.
+
+    @abstractmethod
+    def get_arrvied_neutrino_flux_IO_NU_X_BAR(self, t, Ev):
+        return 0.
+
 
 
 
--- simulation/python/flux/Nakazato_2013_Model.py
+++ simulation/python/flux/Nakazato_2013_Model.py
@@ -2,6 +2,10 @@ from flux.CCSNeModel import CCSNeModel
 
 from astropy import units as u
 from snewpy.neutrino import Flavor, MassHierarchy
+from snewpy.flavor_transformation import NoTransformation, AdiabaticMSW, ThreeFlavorDecoherence
+
+import warnings
+warnings.filterwarnings("ignore")
 
 
 class Nakazato_2013_Model(CCSNeModel):
@@ -25,25 +29,167 @@ class Nakazato_2013_Model(CCSNeModel):
         self.model = Nakazato_2013(self.model_file)
 
 
-    def get_flux_ET(self, t, Ev, flavor):
+    def get_flux_ET_NU_E(self, t, Ev):
         """
         get luminosity (unit: [erg-1s-1]) at time t (unit: s) and neutrino energy E (unit: MeV) w/o flavor conversion
         """
         time = t * u.s
         E = Ev * u.MeV
-        flu = self.model.get_initial_spectra(time, E)[flavor].to_value()
+        flu = self.model.get_initial_spectra(time, E)[Flavor.NU_E].to_value()
+        
+        return flu 
+
+    def get_flux_ET_NU_E_BAR(self, t, Ev):
+        """
+        get luminosity (unit: [erg-1s-1]) at time t (unit: s) and neutrino energy E (unit: MeV) w/o flavor conversion
+        """
+        time = t * u.s
+        E = Ev * u.MeV
+        flu = self.model.get_initial_spectra(time, E)[Flavor.NU_E_BAR].to_value()
+        
+        return flu 
+
+    def get_flux_ET_NU_X(self, t, Ev):
+        """
+        get luminosity (unit: [erg-1s-1]) at time t (unit: s) and neutrino energy E (unit: MeV) w/o flavor conversion
+        """
+        time = t * u.s
+        E = Ev * u.MeV
+        flu = self.model.get_initial_spectra(time, E)[Flavor.NU_X].to_value()
+        
+        return flu 
+
+    def get_flux_ET_NU_X_BAR(self, t, Ev):
+        """
+        get luminosity (unit: [erg-1s-1]) at time t (unit: s) and neutrino energy E (unit: MeV) w/o flavor conversion
+        """
+        time = t * u.s
+        E = Ev * u.MeV
+        flu = self.model.get_initial_spectra(time, E)[Flavor.NU_X_BAR].to_value()
         
         return flu 
 
 
-    def get_arrvied_neutrino_flux(self, t, Ev, distance, flavor, transfrom):
+    def get_arrvied_neutrino_flux_noosc_NU_E(self, t, Ev):
         """
         get the neutrino number (unit: /(cm^2*erg*s)) at time t and neutrino energy Ev from distance away.
         """
+        transfrom = NoTransformation()
         time = t * u.s
         E = Ev * u.MeV
-        return self.model.get_flux(time, E, distance, flavor_xform=transfrom)
+        distance = 10.  # unit: kpc
+        return self.model.get_flux(time, E, distance, flavor_xform=transfrom)[Flavor.NU_E]
+
+    def get_arrvied_neutrino_flux_noosc_NU_E_BAR(self, t, Ev):
+        """
+        get the neutrino number (unit: /(cm^2*erg*s)) at time t and neutrino energy Ev from distance away.
+        """
+        transfrom = NoTransformation()
+        time = t * u.s
+        E = Ev * u.MeV
+        distance = 10.  # unit: kpc
+        return self.model.get_flux(time, E, distance, flavor_xform=transfrom)[Flavor.NU_E_BAR]
+
+    def get_arrvied_neutrino_flux_noosc_NU_X(self, t, Ev):
+        """
+        get the neutrino number (unit: /(cm^2*erg*s)) at time t and neutrino energy Ev from distance away.
+        """
+        transfrom = NoTransformation()
+        time = t * u.s
+        E = Ev * u.MeV
+        distance = 10.  # unit: kpc
+        return self.model.get_flux(time, E, distance, flavor_xform=transfrom)[Flavor.NU_X]
+
+    def get_arrvied_neutrino_flux_noosc_NU_X_BAR(self, t, Ev):
+        """
+        get the neutrino number (unit: /(cm^2*erg*s)) at time t and neutrino energy Ev from distance away.
+        """
+        transfrom = NoTransformation()
+        time = t * u.s
+        E = Ev * u.MeV
+        distance = 10.  # unit: kpc
+        return self.model.get_flux(time, E, distance, flavor_xform=transfrom)[Flavor.NU_X_BAR]
 
 
 
+    def get_arrvied_neutrino_flux_NO_NU_E(self, t, Ev):
+        """
+        get the neutrino number (unit: /(cm^2*erg*s)) at time t and neutrino energy Ev from distance away.
+        """
+        nmo_trans = AdiabaticMSW(mh=MassHierarchy.NORMAL)
+        time = t * u.s
+        E = Ev * u.MeV
+        distance = 10.  # unit: kpc
+        return self.model.get_flux(time, E, distance, flavor_xform=nmo_trans)[Flavor.NU_E]
+
+    def get_arrvied_neutrino_flux_NO_NU_E_BAR(self, t, Ev):
+        """
+        get the neutrino number (unit: /(cm^2*erg*s)) at time t and neutrino energy Ev from distance away.
+        """
+        nmo_trans = AdiabaticMSW(mh=MassHierarchy.NORMAL)
+        time = t * u.s
+        E = Ev * u.MeV
+        distance = 10.  # unit: kpc
+        return self.model.get_flux(time, E, distance, flavor_xform=nmo_trans)[Flavor.NU_E_BAR]
+
+    def get_arrvied_neutrino_flux_NO_NU_X(self, t, Ev):
+        """
+        get the neutrino number (unit: /(cm^2*erg*s)) at time t and neutrino energy Ev from distance away.
+        """
+        nmo_trans = AdiabaticMSW(mh=MassHierarchy.NORMAL)
+        time = t * u.s
+        E = Ev * u.MeV
+        distance = 10.  # unit: kpc
+        return self.model.get_flux(time, E, distance, flavor_xform=nmo_trans)[Flavor.NU_X]
+
+    def get_arrvied_neutrino_flux_NO_NU_X_BAR(self, t, Ev):
+        """
+        get the neutrino number (unit: /(cm^2*erg*s)) at time t and neutrino energy Ev from distance away.
+        """
+        nmo_trans = AdiabaticMSW(mh=MassHierarchy.NORMAL)
+        time = t * u.s
+        E = Ev * u.MeV
+        distance = 10.  # unit: kpc
+        return self.model.get_flux(time, E, distance, flavor_xform=nmo_trans)[Flavor.NU_X_BAR]
+
+
+    def get_arrvied_neutrino_flux_IO_NU_E(self, t, Ev):
+        """
+        get the neutrino number (unit: /(cm^2*erg*s)) at time t and neutrino energy Ev from distance away.
+        """
+        nmo_trans = AdiabaticMSW(mh=MassHierarchy.INVERTED)
+        time = t * u.s
+        E = Ev * u.MeV
+        distance = 10.  # unit: kpc
+        return self.model.get_flux(time, E, distance, flavor_xform=nmo_trans)[Flavor.NU_E]
+
+    def get_arrvied_neutrino_flux_IO_NU_E_BAR(self, t, Ev):
+        """
+        get the neutrino number (unit: /(cm^2*erg*s)) at time t and neutrino energy Ev from distance away.
+        """
+        nmo_trans = AdiabaticMSW(mh=MassHierarchy.INVERTED)
+        time = t * u.s
+        E = Ev * u.MeV
+        distance = 10.  # unit: kpc
+        return self.model.get_flux(time, E, distance, flavor_xform=nmo_trans)[Flavor.NU_E_BAR]
+
+    def get_arrvied_neutrino_flux_IO_NU_X(self, t, Ev):
+        """
+        get the neutrino number (unit: /(cm^2*erg*s)) at time t and neutrino energy Ev from distance away.
+        """
+        nmo_trans = AdiabaticMSW(mh=MassHierarchy.INVERTED)
+        time = t * u.s
+        E = Ev * u.MeV
+        distance = 10.  # unit: kpc
+        return self.model.get_flux(time, E, distance, flavor_xform=nmo_trans)[Flavor.NU_X]
+
+    def get_arrvied_neutrino_flux_IO_NU_X_BAR(self, t, Ev):
+        """
+        get the neutrino number (unit: /(cm^2*erg*s)) at time t and neutrino energy Ev from distance away.
+        """
+        nmo_trans = AdiabaticMSW(mh=MassHierarchy.INVERTED)
+        time = t * u.s
+        E = Ev * u.MeV
+        distance = 10.  # unit: kpc
+        return self.model.get_flux(time, E, distance, flavor_xform=nmo_trans)[Flavor.NU_X_BAR]
 
