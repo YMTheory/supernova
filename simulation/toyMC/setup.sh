4f6ea2d5e5fdc2d8c25456dd3921bd770c4c6cfb
YMTheory <932899789@qq.com>
2023-05-05 19:47:43 +0800

    set environment variables

---
+++ simulation/toyMC/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+export PDF1DFILEPATH=/junofs/users/miaoyu/supernova/simulation/C++/jobs/
+export PDF2DFILEPATH=/junofs/users/miaoyu/supernova/simulation/C++/PDFs2d/
+export FITTERPATH=/junofs/users/miaoyu/supernova/simulation/toyMC/
+export MASSASIMOVJOBPATH=/junofs/users/miaoyu/supernova/simulation/toyMC/absMass_jobs/
+export MOASIMOVJOBPATH=/junofs/users/miaoyu/supernova/simulation/toyMC/asimov_jobs
