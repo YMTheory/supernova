6af10685f81f2f9e65a0ff36837411c2e4f7b110
YMTheory <932899789@qq.com>
2023-06-04 22:26:15 +0800

    update environment variables.

--- simulation/toyMC/setup.sh
+++ simulation/toyMC/setup.sh
@@ -1,7 +1,12 @@
 #!/bin/zsh
-
+echo "Source environment variables script."
 export PDF1DFILEPATH=/junofs/users/miaoyu/supernova/simulation/C++/jobs/
+export PDF1DFILEPATHNEW=/junofs/users/miaoyu/supernova/simulation/C++/PDFs1d/
 export PDF2DFILEPATH=/junofs/users/miaoyu/supernova/simulation/C++/PDFs2d/
+export PDF2DFILEPATHNEW=/junofs/users/miaoyu/supernova/simulation/C++/PDFs2d/Res3_newpESXS/
 export FITTERPATH=/junofs/users/miaoyu/supernova/simulation/toyMC/
 export MASSASIMOVJOBPATH=/junofs/users/miaoyu/supernova/simulation/toyMC/absMass_jobs/
-export MOASIMOVJOBPATH=/junofs/users/miaoyu/supernova/simulation/toyMC/asimov_jobs
+export MOASIMOVJOBPATH=/junofs/users/miaoyu/supernova/simulation/toyMC/asimov_jobs/
+export DATA1DFILEPATH=/junofs/users/miaoyu/supernova/simulation/toyMC/Data1d/
+export DATA2DFILEPATH=/junofs/users/miaoyu/supernova/simulation/toyMC/Data2d/
+export CSVFILEPATH=/junofs/users/miaoyu/supernova/simulation/toyMC/results/
